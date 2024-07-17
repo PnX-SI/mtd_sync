419d549f6a323cb8f63d3c55eb29d83b0464fc5c
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2024-07-17 11:24:08 +0200

    feat(command): add missing command

--- src/mtd_sync/blueprint.py
+++ src/mtd_sync/blueprint.py
@@ -1,4 +1,5 @@
 from flask import request, g, current_app, Blueprint
+import click
 import logging
 from flask_login import login_required, login_manager
 from geonature.core.gn_meta.routes import routes
@@ -23,3 +24,28 @@ def synchronize_mtd():
             sync_af_and_ds_by_user(id_role=current_user.id_role)
         except Exception as e:
             log.exception("Error while get JDD via MTD")
+
+
+@blueprint.cli.command()
+@click.option("--id-role", nargs=1, required=False, default=None, help="ID of an user")
+@click.option(
+    "--id-af",
+    nargs=1,
+    required=False,
+    default=None,
+    help="ID of an acquisition framework",
+)
+def mtd_sync(id_role, id_af):
+    """
+    \b
+    Triggers :
+    - global sync for instance
+    - a sync for a given user only (if id_role is provided)
+    - a sync for a given AF (Acquisition Framework) only (if id_af is provided). NOTE: the AF should in this case already exist in the database, and only datasets associated to this AF will be retrieved
+
+    NOTE: if both id_role and id_af are provided, only the datasets possibly associated to both the AF and the user will be retrieved.
+    """
+    if id_role:
+        return sync_af_and_ds_by_user(id_role, id_af)
+    else:
+        return mtd_sync_af_and_ds()
