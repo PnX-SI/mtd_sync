bb51236b1c6b336b6594450991a13b8a434702e0
Pierre Narcisi <pierre.narcisi@mnhn.fr>
2024-08-23 16:51:53 +0200

    fix(tests) add config file

--- src/mtd_sync/conf_schema_toml.py
+++ src/mtd_sync/conf_schema_toml.py
@@ -2,17 +2,15 @@ from marshmallow import Schema, fields, post_load
 
 
 class GnModuleSchemaConf(Schema):
-    BASE_URL = fields.String(required=True)
+    BASE_URL = fields.String()
     XML_NAMESPACE = fields.String(load_default="{http://inpn.mnhn.fr/mtd}")
-    USER = fields.String(required=True)
-    PASSWORD = fields.String(required=True)
+    USER = fields.String()
+    PASSWORD = fields.String()
     ID_INSTANCE_FILTER = fields.Integer(load_default=None)
     MTD_API_ENDPOINT = fields.Url(load_default="https://preprod-inpn.mnhn.fr/mtd")
     SYNC_LOG_LEVEL = fields.String(load_default="INFO")
     USERS_CAN_SEE_ORGANISM_DATA = False
-    JDD_MODULE_CODE_ASSOCIATION = fields.List(
-        fields.String, load_default=["OCCTAX", "OCCHAB"]
-    )
+    JDD_MODULE_CODE_ASSOCIATION = fields.List(fields.String, load_default=["OCCTAX", "OCCHAB"])
     ID_PROVIDER_INPN = fields.String(load_default="cas_inpn")
     ID_USER_SOCLE_1 = fields.Integer(load_default=1)
     ID_USER_SOCLE_2 = fields.Integer(load_default=2)
