25a42c7df15ff7650567a3b1c268dcc904b58164
Pierre-Narcisi <pierre.narcisi@mnhn.fr>
2025-04-29 17:18:03 +0200

    feat(github-actions): add lint and pytest github actions worfklows
    Add two GitHub workflows - GitHub Actions:
    - `lint.yml`: Backend code formatting action using Black / tests .py files are in `src/mtd_sync/tests/`
    - `pytest.yml`: Backend tests execution action using PyTest + Upload of code coverage using Codecov
    
    Add a first dumb test to verify execution of pytest action:
    - `test_mtd.py`
    
    Other added files are necessary to complete actions and tests execution.
    
    From PR #3: https://github.com/PnX-SI/mtd_sync/pull/3/commits

---
+++ src/mtd_sync/test_config.py
@@ -0,0 +1,18 @@
+CELERY = {
+    "broker_url": "redis://localhost:6379/0",
+    "result_backend": "redis://localhost:6379/0",
+}
+
+
+BASE_URL = "lalala"
+XML_NAMESPACE = "{http://inpn.mnhn.fr/mtd}"
+USER = "lalala"
+PASSWORD = "lalala"
+ID_INSTANCE_FILTER = 0
+MTD_API_ENDPOINT = "https://preprod-inpn.mnhn.fr/mtd"
+SYNC_LOG_LEVEL = "INFO"
+USERS_CAN_SEE_ORGANISM_DATA = False
+JDD_MODULE_CODE_ASSOCIATION = ["OCCTAX", "OCCHAB"]
+ID_PROVIDER = "cas_inpn"
+ID_USER_SOCLE_1 = 1
+ID_USER_SOCLE_2 = 2
