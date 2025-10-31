72a3465bb98ba15330a087c813bf9059b5b99cac
Christophe Ramet <christophe.ramet@mnhn.fr>
2025-10-31 14:00:14 +0100

    feat(frontend): Added folder information in frontend

--- frontend/app/components/module/depobio.component.ts
+++ frontend/app/components/module/depobio.component.ts
@@ -1,41 +1,55 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {DemarcheSimplifieeService} from "../../services/demarche-simplifiee.service";
 
 @Component({
-  selector: 'app-depobio',
-  templateUrl: './depobio.component.html',
-  styleUrls: ['./depobio.component.css']
+    selector: 'app-depobio',
+    templateUrl: './depobio.component.html',
+    styleUrls: ['./depobio.component.css']
 })
 export class DepobioComponent {
-  numeroDossier: string = '';
-  message: string = '';
-  isError: boolean = false;
-  isLoading: boolean = false;
+    folderNumber: string = '';
+    message: string = '';
+    isError: boolean = false;
+    isLoading: boolean = false;
+    folderData: any = null;
 
-  constructor(private demarcheSimplifieeService: DemarcheSimplifieeService) {}
-
-  verifierDossier() {
-    if (!this.numeroDossier) {
-      return;
+    constructor(private demarcheSimplifieeService: DemarcheSimplifieeService) {
     }
 
-    this.message = '';
-    this.isError = false;
-    this.isLoading = true;
+    getFolder() {
+        if (!this.folderNumber) {
+            return;
+        }
 
-    this.demarcheSimplifieeService.verifierDossier(this.numeroDossier).subscribe({
-      next: () => {
-        this.message = 'Le dossier existe dans démarche simplifiée.';
+        this.message = '';
         this.isError = false;
-        this.isLoading = false;
-      },
-      error: (error) => {
-        this.message = error.status === 404
-          ? 'Le dossier n\'existe pas dans démarche simplifiée.'
-          : 'Une erreur est survenue lors de la vérification du dossier.';
-        this.isError = true;
-        this.isLoading = false;
-      }
-    });
-  }
+        this.isLoading = true;
+        this.folderData = null;
+        this.demarcheSimplifieeService.getFolder(this.folderNumber).subscribe({
+            next: (data) => {
+                this.folderData = data
+                this.message = 'Le dossier a été trouvé dans démarches simplifiées.';
+                this.isError = false;
+                this.isLoading = false;
+            },
+            error: (error) => {
+                this.message = error.status === 404
+                    ? 'Le dossier n\'existe pas dans démarches simplifiées.'
+                    : 'Une erreur est survenue lors de la vérification du dossier.';
+                this.isError = true;
+                this.isLoading = false;
+                this.folderData = null;
+            }
+        });
+    }
+
+    createAF() {
+        try {
+            const apiUrl = this.demarcheSimplifieeService.createAFUrl(this.folderData);
+            window.location.href = apiUrl; // Redirige dans le même onglet
+        } catch (error) {
+            console.error('Erreur lors de la génération de l\'URL:', error);
+        }
+    }
+
 }
--- frontend/app/services/demarche-simplifiee.service.ts
+++ frontend/app/services/demarche-simplifiee.service.ts
@@ -1,19 +1,35 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { ConfigService } from '@geonature/services/config.service';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ConfigService} from '@geonature/services/config.service';
+import {Observable} from 'rxjs';
+import {ModuleService} from '@geonature/services/module.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class DemarcheSimplifieeService {
-  constructor(
-    private http: HttpClient,
-    private config: ConfigService
-  ) {}
+    constructor(
+        private http: HttpClient,
+        private config: ConfigService,
+        private module: ModuleService
+    ) {
+    }
 
-  verifierDossier(numeroDossier: string): Observable<any> {
-    const apiUrl = `${this.config.API_ENDPOINT}/${this.config.PLUGIN_DEPOBIO.MODULE_URL}/validate_folder_number/${numeroDossier}`;
-    return this.http.get<any>(apiUrl);
-  }
+    getFolder(folderNumber: string): Observable<any> {
+        const apiUrl = `${this.config.API_ENDPOINT}/${this.config.PLUGIN_DEPOBIO.MODULE_URL}/get_folder/${folderNumber}`;
+        return this.http.get<any>(apiUrl);
+    }
+
+    createAFUrl(folderData: any): string {
+        const params = new URLSearchParams({
+            id: folderData.id,
+            libelle: folderData.libelle,
+            description: folderData.description || '',
+            number: folderData.number,
+            state: folderData.state,
+            date_fin: folderData.date_fin || ''
+        });
+        return `${this.config.URL_APPLICATION}/#/${this.module.getModule("METADATA").module_url}/af?${params.toString()}`;
+    }
+
 }
